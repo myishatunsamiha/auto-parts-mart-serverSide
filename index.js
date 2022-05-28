9007af7160560b185585c0d32eec42c2bceec0a1
myishatunsamiha <myisha.samiha@gmail.com>
2022-05-29 01:09:59 +0600

    first

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 app.use(cors());
